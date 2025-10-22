8e22a2173e33cff5b322d88c8ac11d5fe2a36d19
vitamorozuk055-ui <vitamorozuk055@gmail.com>
2025-10-22 16:30:16 -0700

    Added main code of the website

---
+++ script.js
@@ -0,0 +1,16 @@
+const controls = document.querySelector(".controls");
+
+controls.addEventListener("click", (e) => {
+  if (e.target === e.currentTarget) {
+    return;
+  }
+
+  const prevActivePane = document.querySelector(".pane.is-active");
+  if (prevActivePane) {
+    prevActivePane.classList.remove("is-active");
+  }
+
+  const paneId = e.target.dataset.id;
+  const nextActivePane = document.querySelector(`#${paneId}`);
+  nextActivePane.classList.add("is-active");
+});
